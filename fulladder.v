9bc181e519232f1b0929c56b70d6628219bd9bef
guava <yuvayashvin@gmail.com>
2024-01-24 12:21:04 +0530

    merged adder files

--- fulladder.v
+++ fulladder.v
@@ -1,3 +1,8 @@
+module half_adder(input a,b, output sum,carry);
+xor m1(sum,a,b);
+and m2(carry,a,b);
+endmodule
+
 module full_adder(input a,b,cin,output sum,carry);
 wire c,c1,s;
 half_adder h1(a,b,s,c);
--- halfadder.v
+++
@@ -1,4 +0,0 @@
-module half_adder(input a,b, output sum,carry);
-xor m1(sum,a,b);
-and m2(carry,a,b);
-endmodule
